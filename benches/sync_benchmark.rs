613eb971acda1bd7ecfcaffbf5cea0b615881516
Brooks Prumo <brooks@solana.com>
2022-06-17 13:18:17 -0500

    Use `iter_batched` to *not* benchmark spawning the threads (#4)

--- benches/sync_benchmark.rs
+++ benches/sync_benchmark.rs
@@ -1,16 +1,11 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc,
 };
 use sync_benchmarking::*;
 
-// bprumo TODO: things to benchmark
-// all readers vs all writers
-// num threads contending
-//      - 2 vs 4 vs 8 vs 16
-// contending as fast as possible vs infrequent
-// size of data behind the lock
+const AMOUNT_OF_WORK: usize = 16_000;
 
 fn bench_small(c: &mut Criterion) {
     let mut group = c.benchmark_group("small");
@@ -18,22 +13,146 @@ fn bench_small(c: &mut Criterion) {
         group.bench_with_input(
             BenchmarkId::new("std::mutex", num_threads),
             num_threads,
-            |b, num_threads| b.iter(|| bench_std_mutex::<Small>(*num_threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(std::sync::Mutex::new(Small::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.lock().unwrap();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.lock().unwrap().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
         group.bench_with_input(
-            BenchmarkId::new("parking_log::mutex", num_threads),
+            BenchmarkId::new("parking_lot::mutex", num_threads),
             num_threads,
-            |b, num_threads| b.iter(|| bench_parking_lot_mutex::<Small>(*num_threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(parking_lot::Mutex::new(Small::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.lock();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.lock().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
         group.bench_with_input(
             BenchmarkId::new("std::rwlock", num_threads),
             num_threads,
-            |b, num_threads| b.iter(|| bench_std_rwlock::<Small>(*num_threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(std::sync::RwLock::new(Small::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.write().unwrap();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.read().unwrap().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
         group.bench_with_input(
             BenchmarkId::new("parking_lot::rwlock", num_threads),
             num_threads,
-            |b, num_threads| b.iter(|| bench_parking_lot_rwlock::<Small>(*num_threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(parking_lot::RwLock::new(Small::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.write();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.read().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
     }
 }
@@ -44,124 +163,148 @@ fn bench_large(c: &mut Criterion) {
         group.bench_with_input(
             BenchmarkId::new("std::mutex", num_threads),
             num_threads,
-            |b, threads| b.iter(|| bench_std_mutex::<Large>(*threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(std::sync::Mutex::new(Large::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.lock().unwrap();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.lock().unwrap().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
         group.bench_with_input(
-            BenchmarkId::new("parking_log::mutex", num_threads),
+            BenchmarkId::new("parking_lot::mutex", num_threads),
             num_threads,
-            |b, threads| b.iter(|| bench_parking_lot_mutex::<Large>(*threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(parking_lot::Mutex::new(Large::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.lock();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.lock().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
         group.bench_with_input(
             BenchmarkId::new("std::rwlock", num_threads),
             num_threads,
-            |b, threads| b.iter(|| bench_std_rwlock::<Large>(*threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(std::sync::RwLock::new(Large::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.write().unwrap();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.read().unwrap().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
         group.bench_with_input(
             BenchmarkId::new("parking_lot::rwlock", num_threads),
             num_threads,
-            |b, threads| b.iter(|| bench_parking_lot_rwlock::<Large>(*threads)),
+            |b, &num_threads| {
+                b.iter_batched(
+                    || {
+                        let x = Arc::new(parking_lot::RwLock::new(Large::default()));
+                        let go = Arc::new(AtomicBool::new(false));
+                        let threads: Vec<_> = (0..num_threads)
+                            .into_iter()
+                            .map(|_| {
+                                let x = Arc::clone(&x);
+                                let go = Arc::clone(&go);
+                                let amount_of_work_per_thread = AMOUNT_OF_WORK / num_threads;
+                                std::thread::spawn(move || {
+                                    while !go.load(Ordering::Acquire) {}
+                                    for _ in 0..amount_of_work_per_thread {
+                                        let mut x = x.write();
+                                        *x.counter_mut() += 1;
+                                    }
+                                })
+                            })
+                            .collect();
+                        (x, go, threads)
+                    },
+                    |(x, go, threads)| {
+                        go.store(true, Ordering::Release);
+                        threads
+                            .into_iter()
+                            .for_each(|thread| thread.join().unwrap());
+                        assert_eq!(x.read().counter() as usize, AMOUNT_OF_WORK);
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
         );
     }
-}
-
-fn bench_std_mutex<T: 'static + Default + Counter + Send>(num_threads: usize) {
-    let amount_of_work = 16_000;
-    let amount_of_work_per_thread = amount_of_work / num_threads;
-    let t = Arc::new(std::sync::Mutex::new(T::default()));
-
-    // bprumo TODO: don't want to test the time it takes to spawn the threads...
-
-    let go = Arc::new(AtomicBool::new(false));
-    let threads = (0..num_threads).into_iter().map(|_| {
-        let t = Arc::clone(&t);
-        let go = Arc::clone(&go);
-        std::thread::spawn(move || {
-            while !go.load(Ordering::Acquire) {}
-            for _ in 0..amount_of_work_per_thread {
-                let mut t = t.lock().unwrap();
-                *t.counter_mut() += 1;
-            }
-        })
-    });
-    go.store(true, Ordering::Release);
-
-    threads.for_each(|thread| thread.join().unwrap());
-    assert_eq!(t.lock().unwrap().counter() as usize, amount_of_work);
-}
-
-fn bench_parking_lot_mutex<T: 'static + Default + Counter + Send>(num_threads: usize) {
-    let amount_of_work = 16_000;
-    let amount_of_work_per_thread = amount_of_work / num_threads;
-    let t = Arc::new(parking_lot::Mutex::new(T::default()));
-
-    // bprumo TODO: don't want to test the time it takes to spawn the threads...
-
-    let go = Arc::new(AtomicBool::new(false));
-    let threads = (0..num_threads).into_iter().map(|_| {
-        let t = Arc::clone(&t);
-        let go = Arc::clone(&go);
-        std::thread::spawn(move || {
-            while !go.load(Ordering::Acquire) {}
-            for _ in 0..amount_of_work_per_thread {
-                let mut t = t.lock();
-                *t.counter_mut() += 1;
-            }
-        })
-    });
-    go.store(true, Ordering::Release);
-
-    threads.for_each(|thread| thread.join().unwrap());
-    assert_eq!(t.lock().counter() as usize, amount_of_work);
-}
-
-fn bench_std_rwlock<T: 'static + Default + Counter + Send + Sync>(num_threads: usize) {
-    let amount_of_work = 16_000;
-    let amount_of_work_per_thread = amount_of_work / num_threads;
-    let t = Arc::new(std::sync::RwLock::new(T::default()));
-
-    // bprumo TODO: don't want to test the time it takes to spawn the threads...
-
-    let go = Arc::new(AtomicBool::new(false));
-    let threads = (0..num_threads).into_iter().map(|_| {
-        let t = Arc::clone(&t);
-        let go = Arc::clone(&go);
-        std::thread::spawn(move || {
-            while !go.load(Ordering::Acquire) {}
-            for _ in 0..amount_of_work_per_thread {
-                let mut t = t.write().unwrap();
-                *t.counter_mut() += 1;
-            }
-        })
-    });
-    go.store(true, Ordering::Release);
-
-    threads.for_each(|thread| thread.join().unwrap());
-    assert_eq!(t.read().unwrap().counter() as usize, amount_of_work);
-}
-
-fn bench_parking_lot_rwlock<T: 'static + Default + Counter + Send + Sync>(num_threads: usize) {
-    let amount_of_work = 16_000;
-    let amount_of_work_per_thread = amount_of_work / num_threads;
-    let t = Arc::new(parking_lot::RwLock::new(T::default()));
-
-    // bprumo TODO: don't want to test the time it takes to spawn the threads...
-
-    let go = Arc::new(AtomicBool::new(false));
-    let threads = (0..num_threads).into_iter().map(|_| {
-        let t = Arc::clone(&t);
-        let go = Arc::clone(&go);
-        std::thread::spawn(move || {
-            while !go.load(Ordering::Acquire) {}
-            for _ in 0..amount_of_work_per_thread {
-                let mut t = t.write();
-                *t.counter_mut() += 1;
-            }
-        })
-    });
-    go.store(true, Ordering::Release);
-
-    threads.for_each(|thread| thread.join().unwrap());
-    assert_eq!(t.read().counter() as usize, amount_of_work);
 }
 
 criterion_group!(benches, bench_small, bench_large);
